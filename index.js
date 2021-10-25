8f1c5274f0c2cca8b71178ff208dde9eaf2d4f3a
Drew Schmidtke <amschmidtke@gmail.com>
2021-10-25 19:15:05 -0400

    initial inquirer menu

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+const consoleTable = require('console.table');
 
-
-
+// start database connetion and main
 db.connect(err => {
     if (err) throw err;
-    console.log('Database connected.');
+    console.log('Database connected. Welcome to Employee Tracker!');
+    employeeTracker();
 });
+
+// main function
+function employeeTracker() {
+    inquirer.prompt(
+        {
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View Departments', 'View Roles', 'View Employees', new inquirer.Separator(), 'Add Department', 'Add Role', 'Add Employee', 'Edit Employee', new inquirer.Separator()]
+        }
+    )
+};
