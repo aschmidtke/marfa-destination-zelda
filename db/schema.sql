b2aec7eb2898743ca92662095c43fd63080e73b8
Drew Schmidtke <amschmidtke@gmail.com>
2021-10-25 19:35:45 -0400

    updated tables per assignment

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -10,17 +10,17 @@ CREATE TABLE departments (
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(20) NOT NULL,
-    department INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
-    salary INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    salary INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
  );
 
  CREATE TABLE employees (
      id INTEGER AUTO_INCREMENT PRIMARY KEY,
      first_name VARCHAR(20) NOT NULL,
      last_name VARCHAR(20) NOT NULL,
-     department INTEGER,
      role_id INTEGER NOT NULL,
-     salary INTEGER NOT NULL,
-     manager VARCHAR(30)
+     manager_id INTEGER NULL,
+     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
  );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,24 +4,24 @@ VALUES
 ('Front of House'),
 ('Back of House');
 
-INSERT INTO roles (title, department, role_id, salary)
+INSERT INTO roles (title, department_id, salary)
 VALUES
-('Manager', 1, 1, 60),
-('Server', 2, 2, 50),
-('Host', 2, 3, 40),
-('Cook', 3, 4, 50),
-('Dishwasher', 3, 5, 40);
+('Manager', 1, 60),
+('Server', 2, 50),
+('Host', 2, 40),
+('Cook', 3, 50),
+('Dishwasher', 3, 40);
 
-INSERT INTO employees (first_name, last_name, department, role_id, salary, manager)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-('Kathy', 'Martin', 1, 1, 60, NULL),
-('Ronny', 'Lora', 1, 1, 60, NULL),
-('Alberta', 'McBride', 2, 2, 50, 'Kathy Martin'),
-('Alice', 'Hoffman', 2, 2, 50, 'Kathy Martin'),
-('Derrick', 'Franklin', 2, 2, 50, 'Kathy Martin'),
-('Andre', 'Talbot', 2, 3, 40, 'Ronny Lora'),
-('David', 'Ragland', 2, 3, 40, 'Ronny Lora'),
-('Mildred', 'Randolph', 3, 4, 50, 'Kathy Martin'),
-('Anthony', 'Millan', 3, 4, 50, 'Kathy Martin'),
-('Cindy', 'Anton', 3, 5, 40, 'Kathy Martin');
+('Kathy', 'Martin', 1, NULL),
+('Ronny', 'Lora', 1, 1),
+('Alberta', 'McBride', 2, 1),
+('Alice', 'Hoffman', 2, 1),
+('Derrick', 'Franklin', 2, 1),
+('Andre', 'Talbot', 3, 2),
+('David', 'Ragland', 3, 2),
+('Mildred', 'Randolph', 4, 2),
+('Anthony', 'Millan', 4, 2),
+('Cindy', 'Anton', 5, 1);
 
