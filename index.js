f0a0e55fa8c372cde688da06560cf8dc28e59043
Drew Schmidtke <amschmidtke@gmail.com>
2021-10-25 20:20:54 -0400

    view funcitons working

--- index.js
+++ index.js
@@ -16,7 +16,74 @@ function employeeTracker() {
             name: 'action',
             type: 'list',
             message: 'What would you like to do?',
-            choices: ['View Departments', 'View Roles', 'View Employees', new inquirer.Separator(), 'Add Department', 'Add Role', 'Add Employee', 'Edit Employee', new inquirer.Separator()]
+            choices: ['View Departments', 'View Roles', 'View Employees', new inquirer.Separator(), 'Add Department', 'Add Role', 'Add Employee', 'Edit Employee', new inquirer.Separator(), 'Exit', new inquirer.Separator()]
         }
-    )
+    ).then((answer) => {
+        switch (answer.action) {
+            case "View Departments":
+                viewDepartments();
+                break;
+
+            case "View Roles":
+                viewRoles();
+                break;
+            
+            case "View Employees":
+                viewEmployees();
+                break;
+
+            case "Add Department":
+                addDepartment();
+                break;
+            
+            case "Add Roles":
+                addRole();
+                break;
+            
+            case "Add Employee":
+                addEmployee();
+                break;
+            
+            case "Edit Employee":
+                editEmployee();
+                break;
+            
+            case "Exit":
+                db.end();
+                break;
+        }
+    })
 };
+
+const viewDepartments = () => {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log('\n');
+        console.table('Departments', results);
+    });
+    console.log('----------');
+    employeeTracker();
+}
+
+const viewRoles = () => {
+    const sql = `SELECT * FROM roles`;
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log('\n');
+        console.table('Roles', results);
+    });
+    console.log('----------');
+    employeeTracker();
+}
+
+const viewEmployees = () => {
+    const sql = `SELECT * FROM employees`;
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log('\n');
+        console.table('Employees', results);
+    });
+    console.log('----------');
+    employeeTracker();
+}
